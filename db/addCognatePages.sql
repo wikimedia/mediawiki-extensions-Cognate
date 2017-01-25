3e7396105f9c893f700f69cdc9b16074d5147a5e
addshore <addshorewiki@gmail.com>
2017-01-25 09:50:12 +0100

    Adjust index on pages table
    Bug: T149082
    Change-Id: I7d1fb4b8549d8ab3b780c32e86fe4d5d90730a7c

--- db/addCognatePages.sql
+++ db/addCognatePages.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS /*_*/cognate_pages (
   cgpa_site BIGINT SIGNED NOT NULL,
   cgpa_namespace INT NOT NULL,
   cgpa_title BIGINT SIGNED NOT NULL,
-  PRIMARY KEY (cgpa_site, cgpa_namespace, cgpa_title)
+  PRIMARY KEY (cgpa_title, cgpa_namespace, cgpa_site)
   )/*$wgDBTableOptions*/;
