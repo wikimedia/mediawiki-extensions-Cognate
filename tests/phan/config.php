bdc7e92a30278f551227c3eaaed5d5245c748259
Kunal Mehta <legoktm@member.fsf.org>
2018-04-05 13:23:11 -0700

    build: Updating mediawiki/mediawiki-phan-config to 0.2.0
    Change-Id: Ibcf103ab29f635f38bd6b4303480e37ca4a9b741

--- tests/phan/config.php
+++ tests/phan/config.php
@@ -1,9 +1,3 @@
 <?php
 
-$cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-
-// TODO: Remove once using a default config that
-// includes https://gerrit.wikimedia.org/r/#/c/411081/
-$cfg['directory_list'][] = 'maintenance/';
-
-return $cfg;
+return require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
